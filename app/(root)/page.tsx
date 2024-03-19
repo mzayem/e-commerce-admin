54514f4c7dd6ad6626c91bbf15d1fecc89a395a2
mzayem <mzayemazam@gmail.com>
2024-03-19 06:39:14 +0500

    testing

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <button size="default" varaint="outline">
+        cclickme
+      </button>
+    </div>
+  );
+}
