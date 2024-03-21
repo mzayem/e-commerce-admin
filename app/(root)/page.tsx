6f0e202eca5dbbc37a48c35b6c6c85d7903d8286
mzayem <mzayemazam@gmail.com>
2024-03-21 22:56:41 +0500

    dialog triggering

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { useEffect } from "react";
 
 const SetupPage = () => {
   // const storeModal = useStoreModal();
   const onOpen = useStoreModal((state) => state.onOpen);
-  const isOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
 
   useEffect(() => {
     if (!isOpen) {
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,4 +1,4 @@
-"use Client";
+"use client";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
@@ -13,7 +13,7 @@ export const StoreModal = () => {
       isOpen={StoreModal.isOpen}
       onClose={StoreModal.onClose}
     >
-      <p> Future Create Store Form</p>
+      <p>Future Create Store Form</p>
     </Modal>
   );
 };
