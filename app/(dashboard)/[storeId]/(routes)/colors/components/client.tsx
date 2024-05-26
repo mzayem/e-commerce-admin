396c29d83eb915f1ca9608cc11757600f7fc01c5
mzayem <mzayemazam@gmail.com>
2024-05-27 03:26:04 +0500

    created color entity
    created the color componenet for the products varaitions

---
+++ app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { DataTable } from "@/components/ui/data-table";
+
+import { ColorsColumn, columns } from "./columns";
+import { Separator } from "@/components/ui/separator";
+import { ApiList } from "@/components/ui/api-list";
+
+interface ColorClientProps {
+  data: ColorsColumn[];
+}
+
+export function ColorsClient({ data }: ColorClientProps) {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between p-4">
+        <Heading
+          title={`Colors (${data.length})`}
+          description="Mange Colors for your store"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
+          <Plus className="mr-2 h-4 w-4 " />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="name" />
+      <Heading title="API" description="API calls for Colors" />
+      <Separator />
+      <ApiList entityName="colors" entityIdName="colorsId" />
+    </>
+  );
+}
---
+++ app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -0,0 +1,33 @@
+import { format, formatDate } from "date-fns";
+import prismadb from "@/lib/prismadb";
+
+import { ColorsColumn } from "./components/columns";
+import { ColorsClient } from "./components/client";
+
+const ColorsPage = async ({ params }: { params: { storeId: string } }) => {
+  const colors = await prismadb.color.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedColors: ColorsColumn[] = colors.map((item) => ({
+    id: item.id,
+    name: item.name,
+    value: item.value,
+    createdAt: format(item.createdAt, "MMMM do, yy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ColorsClient data={formattedColors} />
+      </div>
+    </div>
+  );
+};
+
+export default ColorsPage;
